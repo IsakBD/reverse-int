c59d20b926b7be54a56afa50b5d627ba26e4daf6
IsakBD <baglan.issak@gmail.com>
2021-03-15 19:14:14 +0600

    solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+        n = n + '';
+        let arr = n.split('');
+        arr = arr.reverse();
+        n = parseInt(arr.join(''));
+        return n;
 }
